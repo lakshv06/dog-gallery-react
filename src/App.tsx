e600f38c7760a27019ccc6a85ff7999d7db83c50
lakshv06 <lakshyaverma0002@gmail.com>
2024-04-23 16:19:38 +0530

    added bpptstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
 function App() {
   return (
     <div className="App">
-      <body>
+      <body className='fs-1'>
       Hello World!
       </body>
     </div>
